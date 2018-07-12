04291178a7024f6cff29992ffbf48c2185052953
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-11 23:17:36 -0400

    Configure API routes for Authentication and Swagger doc. Composer add light/yii2-swagger package.

--- api/config/api-rules/v1.php
+++ api/config/api-rules/v1.php
@@ -8,7 +8,16 @@ $version = "v1";
 
 return [
 
+	// API Documentation
+
+	""                      => "$version/default/doc", // Swagger UI with "Try it out" button
+	"/$version/doc"         => "$version/default/doc", // Swagger UI with "Try it out" button
+	"/$version/schema"      => "$version/default/schema", // Swagger JSON spec file
+
 	// Authentication
+
+	"POST    $version/auth" => "$version/auth/login",
+	"DELETE  $version/auth" => "$version/auth/logout",
 
 	// Orders
 
