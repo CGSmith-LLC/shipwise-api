be78a86b7a5897a95b7e8e1cf94ea7d688944b0c
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-15 00:34:53 -0400

    Prepare Customer Controller methods

--- api/modules/v1/controllers/CustomerController.php
+++ api/modules/v1/controllers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace api\modules\v1\controllers;
 
 use api\modules\v1\components\ControllerEx;
+
 // use api\modules\v1\models\customer\form\CustomerForm;
 
 /**
@@ -13,17 +14,53 @@ use api\modules\v1\components\ControllerEx;
 class CustomerController extends ControllerEx
 {
 	/** @inheritdoc */
-	/*protected function verbs()
+	protected function verbs()
 	{
 		return [
-			"index"       => ["GET", "OPTIONS"],
-			"acknowledge" => ["PUT", "OPTIONS"],
+			'index'  => ['GET'],
+			'create' => ['POST'],
+			'update' => ['PUT'],
+			'view'   => ['GET'],
+			'delete' => ['DELETE'],
 		];
-	}*/
+	}
 
 	/** @inheritdoc */
 	public function actionIndex()
 	{
+		echo 'actionIndex ';
+		print_r($this->apiConsumer->attributes);
+		exit;
+	}
+
+	/** @inheritdoc */
+	public function actionCreate()
+	{
+		echo 'actionCreate ';
+		print_r($this->apiConsumer->attributes);
+		exit;
+	}
+
+	/** @inheritdoc */
+	public function actionView($id)
+	{
+		echo 'actionView id: ' . $id;
+		print_r($this->apiConsumer->attributes);
+		exit;
+	}
+
+	/** @inheritdoc */
+	public function actionUpdate($id)
+	{
+		echo 'actionUpdate id: ' . $id;
+		print_r($this->apiConsumer->attributes);
+		exit;
+	}
+
+	/** @inheritdoc */
+	public function actionDelete($id)
+	{
+		echo 'actionDelete id: ' . $id;
 		print_r($this->apiConsumer->attributes);
 		exit;
 	}
