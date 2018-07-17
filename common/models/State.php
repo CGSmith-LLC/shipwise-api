f19b67f118d86fc366c66b4911c8d60a5b9d4675
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-16 22:11:24 -0400

    Generate States models

---
+++ common/models/State.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace common\models;
+
+use common\models\base\BaseState;
+
+/**
+ * Class State
+ *
+ * @package common\models
+ */
+class State extends BaseState
+{
+
+}
---
+++ common/models/base/BaseState.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models\base;
+
+/**
+ * This is the model class for table "states".
+ *
+ * @property int    $id
+ * @property string $name
+ * @property string $abbreviation
+ */
+class BaseState extends \yii\db\ActiveRecord
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'states';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['name', 'abbreviation'], 'required'],
+			[['name'], 'string', 'max' => 45],
+			[['abbreviation'], 'string', 'max' => 12],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id'           => 'ID',
+			'name'         => 'Name',
+			'abbreviation' => 'Abbreviation',
+		];
+	}
+}
+
+A Product of Yii Software LLC
