45fdeb394b63176a740bb783c3b5ae418c6d2d94
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-17 13:23:02 -0400

    Create extended Item model for API v1.

---
+++ api/modules/v1/models/order/ItemEx.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace api\modules\v1\models\order;
+
+use common\models\Item;
+
+/**
+ * Class ItemEx
+ *
+ * @package api\modules\v1\models\order
+ */
+class ItemEx extends Item
+{
+	/**
+	 * @inheritdoc
+	 *
+	 * @SWG\Definition(
+	 *     definition = "Item",
+	 *
+	 *     @SWG\Property( property = "id",   type = "integer", description = "Item ID" ),
+	 *     @SWG\Property( property = "quantity", type = "integer", description = "Quantity" ),
+	 *     @SWG\Property( property = "sku", type = "integer", description = "SKU" ),
+	 *     @SWG\Property( property = "name", type = "string", description = "Item name" ),
+	 * )
+	 */
+	public function fields()
+	{
+		return ['id', 'quantity', 'sku', 'name'];
+	}
+}
