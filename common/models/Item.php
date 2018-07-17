36f9ffc99881844356e762167dbf1b9aedafc219
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-16 21:57:20 -0400

    Generate Item models

---
+++ common/models/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\models;
+
+use common\models\base\BaseItem;
+
+/**
+ * Class Item`
+ *
+ * @todo Generate relations
+ *
+ * @package common\models
+ */
+class Item extends BaseItem
+{
+
+}
---
+++ common/models/base/BaseItem.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models\base;
+
+/**
+ * This is the model class for table "items".
+ *
+ * @property int    $id
+ * @property int    $order_id
+ * @property int    $quantity
+ * @property string $sku
+ * @property string $name
+ */
+class BaseItem extends \yii\db\ActiveRecord
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'items';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['order_id', 'quantity', 'sku'], 'required'],
+			[['order_id', 'quantity'], 'integer'],
+			[['sku', 'name'], 'string', 'max' => 45],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id'       => 'ID',
+			'order_id' => 'Order ID',
+			'quantity' => 'Quantity',
+			'sku'      => 'Sku',
+			'name'     => 'Name',
+		];
+	}
+}
