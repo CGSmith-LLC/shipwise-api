875aa182e509c71704ff46d1c8a96aff8e0f7cf2
Vitaliy Katerusha <vkaterusha@gmail.com>
2018-07-16 21:49:36 -0400

    Generate Address models

---
+++ common/models/Address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\models;
+
+use common\models\base\BaseAddress;
+
+/**
+ * Class Address
+ *
+ * @todo Generate relations
+ *
+ * @package common\models
+ */
+class Address extends BaseAddress
+{
+
+}
---
+++ common/models/base/BaseAddress.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace common\models\base;
+
+/**
+ * This is the model class for table "addresses".
+ *
+ * @property int    $id
+ * @property string $name
+ * @property string $address1
+ * @property string $address2
+ * @property string $city
+ * @property int    $state_id
+ * @property string $zip
+ * @property string $phone
+ * @property string $notes
+ * @property string $created_date
+ * @property string $updated_date
+ */
+class BaseAddress extends \yii\db\ActiveRecord
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'addresses';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			[['name', 'address1', 'city', 'state_id', 'zip', 'phone'], 'required'],
+			[['state_id'], 'integer'],
+			[['created_date', 'updated_date'], 'safe'],
+			[['name', 'address1', 'address2', 'city'], 'string', 'max' => 64],
+			[['zip'], 'string', 'max' => 16],
+			[['phone'], 'string', 'max' => 32],
+			[['notes'], 'string', 'max' => 140],
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id'           => 'ID',
+			'name'         => 'Name',
+			'address1'     => 'Address1',
+			'address2'     => 'Address2',
+			'city'         => 'City',
+			'state_id'     => 'State ID',
+			'zip'          => 'Zip',
+			'phone'        => 'Phone',
+			'notes'        => 'Notes',
+			'created_date' => 'Created Date',
+			'updated_date' => 'Updated Date',
+		];
+	}
+}
