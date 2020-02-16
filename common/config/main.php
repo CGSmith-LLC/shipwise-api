7f9a7f1efd98411f12a6cb599885acd6288aa57b
vitaliy Katerusha <vkaterusha@gmail.com>
2020-02-16 18:48:47 -0500

    Configurate Yii Queue component

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,9 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'bootstrap' => [
+        'queue', // The component registers its own console commands
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -13,5 +16,15 @@ return [
         'customerSettings' => [
             'class' => 'common\components\CustomerSettings',
         ],
+
+        'queue' => [
+            'class'     => 'yii\queue\db\Queue',
+            'db'        => 'db', // DB connection component or its config
+            'tableName' => '{{%queue}}', // Table name
+            'channel'   => 'default', // Queue channel key
+            'mutex'     => 'yii\mutex\MysqlMutex', // Mutex used to sync queries
+            'as log'    => 'yii\queue\LogBehavior',
+        ],
+
     ],
 ];
