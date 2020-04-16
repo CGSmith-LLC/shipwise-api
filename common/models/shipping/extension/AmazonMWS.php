b60b049b1b0faf3adb547823c22ddf84bb878256
Vitaliy Katerusha <vkaterusha@gmail.com>
2020-04-16 00:05:11 -0400

    Amazon MWS CreateShipment request. WIP

--- common/models/shipping/extension/AmazonMWS.php
+++ common/models/shipping/extension/AmazonMWS.php
@@ -2,15 +2,9 @@
 
 namespace common\models\shipping\extension;
 
-use common\models\{Charge, Money};
+use common\models\{Charge, Item, Money};
 use Yii;
-use common\models\shipping\{
-    Carrier,
-    PackageType,
-    Service,
-    ShipmentPlugin,
-    ShipmentException
-};
+use common\models\shipping\{Carrier, PackageType, Service, Shipment, ShipmentPlugin, ShipmentException};
 
 /**
  * Class AmazonMWS
@@ -166,21 +160,105 @@ class AmazonMWS extends ShipmentPlugin
         /**
          * Build Amazon MWS `CreateShipmentRequest`
          */
-
-        /************************************************************************
-         * Uncomment to try out Mock Service that simulates MWSMerchantFulfillmentService
-         * responses without calling MWSMerchantFulfillmentService service.
-         *
-         * Responses are loaded from local XML files. You can tweak XML files to
-         * experiment with various outputs during development
-         *
-         * XML files available under MWSMerchantFulfillmentService/Mock tree
-         *
-         ***********************************************************************/
-        $service = new \MWSMerchantFulfillmentService_Mock();
-
         $request = new \MWSMerchantFulfillmentService_Model_CreateShipmentRequest();
-        $request->setSellerId('123456');
+        $request->setSellerId($this->sellerId);
+        $request->setMWSAuthToken($this->mwsAuthToken);
+        $request->setShippingServiceId($this->shipment->service->carrier_code ?? null);
+
+        $details = new \MWSMerchantFulfillmentService_Model_ShipmentRequestDetails();
+        $details->setAmazonOrderId($this->shipment->reference1);
+
+        /**
+         * Amazon Items
+         */
+        $itemList = [];
+        /** @var Item[] $orderItems */
+        $orderItems = Item::find()->where(['order_id' => $this->shipment->order_id])->all();
+
+        foreach ($orderItems as $orderItem) {
+            $_item = new \MWSMerchantFulfillmentService_Model_Item();
+            $_item->setOrderItemId($orderItem->uuid);
+            $_item->setQuantity($orderItem->quantity);
+            $itemList[] = $_item;
+        }
+        $details->setItemList($itemList);
+
+        /**
+         * Ship From
+         */
+        $shipFrom = new \MWSMerchantFulfillmentService_Model_Address();
+        $shipFrom->setName(substr(
+            $this->shipment->sender_company ?? $this->shipment->sender_contact,
+            0,
+            30));
+        $shipFrom->setAddressLine1(substr($this->shipment->sender_address1, 0, 180));
+        if (!empty($this->shipment->sender_address2)) {
+            $shipFrom->setAddressLine2(substr($this->shipment->sender_address2, 0, 60));
+        }
+        $shipFrom->setEmail($this->shipment->sender_email);
+        $shipFrom->setCity(substr($this->shipment->sender_city, 0, 30));
+        $shipFrom->setStateOrProvinceCode($this->shipment->sender_state);
+        $shipFrom->setPostalCode($this->shipment->sender_postal_code);
+        $shipFrom->setCountryCode($this->shipment->sender_country);
+        $shipFrom->setPhone(preg_replace("/[^0-9]/", "", $this->shipment->sender_phone));
+        $details->setShipFromAddress($shipFrom);
+
+        /**
+         * Package Dimensions and Weight
+         * @todo To be determined how to handle MPS cases
+         */
+        $value = 5; // @todo for testing purpose only!!!
+        $dims  = new \MWSMerchantFulfillmentService_Model_PackageDimensions();
+        $dims->setLength(round($value, 2));
+        $dims->setWidth(round($value, 2));
+        $dims->setHeight(round($value, 2));
+        $_unit = ($this->shipment->dim_units == Shipment::DIM_UNITS_IN) ? 'inches' : 'centimeters';
+        $dims->setUnit($_unit);
+        $details->setPackageDimensions($dims);
+
+        $weight = new \MWSMerchantFulfillmentService_Model_Weight();
+        if ($this->shipment->weight_units == Shipment::WEIGHT_UNITS_LB) {
+            $_unit  = 'ounces';
+            $_value = $value * 16;
+        } else {
+            $_unit  = 'grams';
+            $_value = $value * 1000;
+        }
+        $weight->setValue(round($_value, 2));
+        $weight->setUnit($_unit);
+        $details->setWeight($weight);
+
+        /**
+         * Shipping Service Options
+         */
+        $options = new \MWSMerchantFulfillmentService_Model_ShippingServiceOptions();
+        /**
+         * DeliveryExperience values:
+         * - DeliveryConfirmationWithAdultSignature - Delivery confirmation with adult signature.
+         * - DeliveryConfirmationWithSignature - Delivery confirmation with signature. Required for DPD (UK).
+         * - DeliveryConfirmationWithoutSignature - Delivery confirmation without signature.
+         * - NoTracking - No delivery confirmation.
+         */
+        $options->setDeliveryExperience('DeliveryConfirmationWithoutSignature'); // @todo Please review for your need
+
+        /**
+         * CarrierWillPickUp
+         * Indicates whether the carrier will pick up the package.
+         * Note: Scheduled carrier pickup is available only using Dynamex (US), DPD (UK), and Royal Mail (UK).
+         */
+        $options->setCarrierWillPickUp('False');
+
+        /**
+         * LabelFormat
+         * The seller's preferred label format.
+         * Note: Not all LabelFormat values are supported by all carriers.
+         * Specifying a LabelFormat value that is not supported by a carrier will filter out shipping service offers
+         * from that carrier.
+         * Must match one of the AvailableLabelFormats returned by GetEligibleShippingServices operation.
+         */
+        $options->setLabelFormat('PDF');
+        $details->setShippingServiceOptions($options);
+        $request->setShipmentRequestDetails($details);
 
         \yii\helpers\VarDumper::dump($request, 10, true);
         exit;
