27bc4ff5753cb59fefabc79977e6b63408990241
Vitaliy Katerusha <vkaterusha@gmail.com>
2020-04-19 01:46:39 -0400

    Finalize AmazonMWS create shipment.

--- common/models/shipping/extension/AmazonMWS.php
+++ common/models/shipping/extension/AmazonMWS.php
@@ -2,9 +2,14 @@
 
 namespace common\models\shipping\extension;
 
-use common\models\{Charge, Item, Money};
+use common\models\Item;
 use Yii;
-use common\models\shipping\{Carrier, PackageType, Service, Shipment, ShipmentPlugin, ShipmentException};
+use common\models\shipping\{
+    Shipment,
+    ShipmentPackage,
+    ShipmentPlugin,
+    ShipmentException
+};
 
 /**
  * Class AmazonMWS
@@ -205,13 +210,14 @@ class AmazonMWS extends ShipmentPlugin
 
         /**
          * Package Dimensions and Weight
-         * @todo To be determined how to handle MPS cases
+         * @todo To be determined how to handle MPS (multiple-piece-shipment) cases
          */
-        $value = 5; // @todo for testing purpose only!!!
-        $dims  = new \MWSMerchantFulfillmentService_Model_PackageDimensions();
-        $dims->setLength(round($value, 2));
-        $dims->setWidth(round($value, 2));
-        $dims->setHeight(round($value, 2));
+        $package = $this->shipment->getPackages()[0] ?? new ShipmentPackage();
+
+        $dims = new \MWSMerchantFulfillmentService_Model_PackageDimensions();
+        $dims->setLength(round($package->length, 2));
+        $dims->setWidth(round($package->width, 2));
+        $dims->setHeight(round($package->height, 2));
         $_unit = ($this->shipment->dim_units == Shipment::DIM_UNITS_IN) ? 'inches' : 'centimeters';
         $dims->setUnit($_unit);
         $details->setPackageDimensions($dims);
@@ -219,10 +225,10 @@ class AmazonMWS extends ShipmentPlugin
         $weight = new \MWSMerchantFulfillmentService_Model_Weight();
         if ($this->shipment->weight_units == Shipment::WEIGHT_UNITS_LB) {
             $_unit  = 'ounces';
-            $_value = $value * 16;
+            $_value = $package->weight * 16;
         } else {
             $_unit  = 'grams';
-            $_value = $value * 1000;
+            $_value = $package->weight * 1000;
         }
         $weight->setValue(round($_value, 2));
         $weight->setUnit($_unit);
@@ -239,7 +245,7 @@ class AmazonMWS extends ShipmentPlugin
          * - DeliveryConfirmationWithoutSignature - Delivery confirmation without signature.
          * - NoTracking - No delivery confirmation.
          */
-        $options->setDeliveryExperience('DeliveryConfirmationWithoutSignature'); // @todo Please review for your need
+        $options->setDeliveryExperience('DeliveryConfirmationWithoutSignature'); // @todo Please review this for your need
 
         /**
          * CarrierWillPickUp
@@ -259,9 +265,7 @@ class AmazonMWS extends ShipmentPlugin
         $options->setLabelFormat('PDF');
         $details->setShippingServiceOptions($options);
         $request->setShipmentRequestDetails($details);
-
-        \yii\helpers\VarDumper::dump($request, 10, true);
-        exit;
+        $this->data = $request;
 
         return $this;
     }
@@ -276,6 +280,55 @@ class AmazonMWS extends ShipmentPlugin
     protected function shipmentExecute()
     {
 
+        // \yii\helpers\VarDumper::dump($this->data, 10, true);
+        // exit;
+
+        $config = [
+            'ServiceURL'    => $this->urlProd,
+            'ProxyHost'     => null,
+            'ProxyPort'     => -1,
+            'ProxyUsername' => null,
+            'ProxyPassword' => null,
+            'MaxErrorRetry' => 3,
+        ];
+
+        $service = new \MWSMerchantFulfillmentService_Client(
+            $this->awsAccessKeyId,
+            $this->awsSecretKey,
+            Yii::$app->name,
+            '1.0',
+            $config);
+
+        /************************************************************************
+         * Uncomment to try out Mock Service that simulates MWSMerchantFulfillmentService
+         * responses without calling MWSMerchantFulfillmentService service.
+         *
+         * Responses are loaded from local XML files. You can tweak XML files to
+         * experiment with various outputs during development
+         *
+         * XML files available under MWSMerchantFulfillmentService/Mock tree
+         ***********************************************************************/
+        //$service = new \MWSMerchantFulfillmentService_Mock();
+
+        try {
+            $this->response = $service->CreateShipment($this->data);
+            Yii::debug($this->response);
+
+        } catch (\MWSMerchantFulfillmentService_Exception $ex) {
+            Yii::debug($ex, 'Amazon MWS Exception');
+
+            $msg = "Caught Exception: " . $ex->getMessage() . "\n";
+            $msg .= "Response Status Code: " . $ex->getStatusCode() . "\n";
+            $msg .= "Error Code: " . $ex->getErrorCode() . "\n";
+            $msg .= "Error Type: " . $ex->getErrorType() . "\n";
+            $msg .= "Request ID: " . $ex->getRequestId() . "\n";
+            //$msg .= "XML: " . $ex->getXML() . "\n");
+            Yii::debug($ex->getXML(), 'Amazon MWS Exception');
+            $msg .= "ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n";
+
+            throw new ShipmentException($msg);
+        }
+
         return $this;
     }
 
@@ -285,11 +338,74 @@ class AmazonMWS extends ShipmentPlugin
      * This method will process the response received from carrier API
      *
      * @return $this
+     * @throws ShipmentException
      * @version 2020.04.15
      *
      */
     protected function shipmentProcess()
     {
+
+        /** @var \MWSMerchantFulfillmentService_Model_CreateShipmentResponse $response */
+        $response = $this->response;
+
+        /** @var \MWSMerchantFulfillmentService_Model_CreateShipmentResult $result */
+        $result = $response->getCreateShipmentResult() ?? null;
+
+        if (!$result instanceof \MWSMerchantFulfillmentService_Model_CreateShipmentResult) {
+            Yii::debug($this->response);
+            throw new ShipmentException('Incorrect response');
+        }
+
+        /** @var \MWSMerchantFulfillmentService_Model_Shipment $shipment */
+        $shipment = $result->getShipment() ?? null;
+
+        /**
+         * Shipment tracking identifier provided by the carrier
+         * @var string $trackingId
+         */
+        $trackingId = $shipment->getTrackingId() ?? null;
+
+        /** @var \MWSMerchantFulfillmentService_Model_Label $label */
+        $label = $shipment->getLabel() ?? null;
+
+        if ($shipment->getStatus() != 'Purchased') {
+            $msg = 'Incorrect shipment status: ' . $shipment->getStatus();
+            Yii::debug($this->response);
+            throw new ShipmentException($msg);
+        }
+
+        /**
+         * SUCCESS Case
+         *
+         * Retrieve tracking and label details from completed shipment reply.
+         */
+
+        /**
+         * @var ShipmentPackage $package
+         * @todo Pending todo MPS (multiple-piece-shipment)
+         */
+        $package = &$this->shipment->getPackages()[0];
+
+        // Tracking details
+        $package->tracking_num        = $trackingId;
+        $package->master_tracking_num = $trackingId;
+        //$package->tracking_url        = "{$this->trackingURL}?tracknum=" . $package->tracking_num;
+
+        /** @var \MWSMerchantFulfillmentService_Model_FileContents $fileContents */
+        $fileContents = $label->getFileContents();
+
+        // Label data
+        $package->label_data   = $fileContents->getContents(); // Base64-encoded data for printing labels, GZip-compressed string
+        $package->label_format = $label->getLabelFormat();
+
+        // Amazon-defined shipment identifier
+        $this->shipment->external_id1 = $shipment->getShipmentId();
+
+        // @todo For now it's always one label/package. For merging multiple files, refer to UPSPlugin code.
+        $this->shipment->mergedLabelsData   = $package->label_data;
+        $this->shipment->mergedLabelsFormat = strtoupper($label->getLabelFormat());
+
+        $this->isShipped = true;
 
         return $this;
     }
