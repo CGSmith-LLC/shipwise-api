7ae303890bd8c5dcbe284f5bf8ffa54dbd29bdd9
Brian Vedzi <brian@cgsmith.net>
2020-05-07 16:00:35 -0500

    onetimes added in invoice run

--- console/controllers/InvoiceController.php
+++ console/controllers/InvoiceController.php
@@ -4,6 +4,7 @@ namespace console\controllers;
 
 use common\models\Invoice;
 use common\models\InvoiceItems;
+use common\models\OneTimeCharge;
 use common\models\Subscription;
 use common\models\SubscriptionItems;
 use yii\console\Controller;
@@ -37,6 +38,7 @@ class InvoiceController extends Controller
             ]);
             $invoice->save();
 
+
             /** @var SubscriptionItems $item */
             foreach ($items as $item) {
                 $invoiceItem = new InvoiceItems([
@@ -47,6 +49,26 @@ class InvoiceController extends Controller
                 $invoiceItem->save();
                 $totalAmount = $totalAmount + $invoiceItem->amount;
             }
+
+
+            // include one time charges
+            foreach (OneTimeCharge::find()
+                         ->where([
+                             'customer_id' => $subscription->customer_id,
+                             'added_to_invoice' => false
+                         ])
+                         ->all() as $onetimecharge) {
+                $invoiceItem = new InvoiceItems([
+                    'name' => $onetimecharge->name,
+                    'amount' => $onetimecharge->amount,
+                    'invoice_id' => $invoice->id,
+                ]);
+                $invoiceItem->save();
+                $totalAmount = $totalAmount + $onetimecharge->amount;
+                $onetimecharge->added_to_invoice = true;
+                $onetimecharge->update();
+            }
+
             $invoice->setAttribute('amount', $totalAmount);
             $invoice->setAttribute('balance', $totalAmount);
             $invoice->update();
@@ -56,6 +78,10 @@ class InvoiceController extends Controller
             $period = new \DateInterval('P' . $subscription->months_to_recur . 'M');
             $subscription->next_invoice = $currentDate->add($period)->format('Y-m-d');
             $subscription->update();
+
+            // send copy of invoice to customer's email address
+
+
         }
 
 
