631da9f1f9d84a5e83eefc8d0a32fde9d6cb2c60
Brian Vedzi <brian@cgsmith.net>
2020-06-02 08:00:22 -0500

    View Page Billing/Invoices styled 1.0

--- frontend/views/payment-method/index.php
+++ frontend/views/payment-method/index.php
@@ -13,10 +13,10 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="payment-method-index">
 
-<h1 style="text-align:center"><?= Html::encode($this->title) ?></h1>
+    <h1 style="text-align:center"><?= Html::encode($this->title) ?></h1>
 
     <div class="container" style="border-style: solid">
-    <?php
+        <?php
         // Display card information
         // Iterate over cards, ask stripe for the payment method details
         /// Display last 4, expiration, and if its default
@@ -25,21 +25,15 @@ $this->params['breadcrumbs'][] = $this->title;
         foreach ($paymentMethodDataProvider->getModels() as $paymentMethod) {
             $stripePaymentMethod = \Stripe\PaymentMethod::retrieve($paymentMethod->stripe_payment_method_id);
             ?>
-            <div class="col-12">
-                <div><?= $stripePaymentMethod->card->brand; ?> <?php
-                    if ($paymentMethod->default === \common\models\PaymentMethod::PRIMARY_PAYMENT_METHOD_YES) { ?>
-                    <span class="label label-primary">Default</span>
-                    <?php
-                    }
-                    ?>
-                </div>
-                <div>**** <?=$stripePaymentMethod->card->last4; ?></div>
-                <div><?=$stripePaymentMethod->card->exp_month . '/' . $stripePaymentMethod->card->exp_year; ?></div>
-                <div>
+            <div class = text-center style="float: left">
+                <div style="padding: 25px">Card Type : <?= $stripePaymentMethod->card->brand; ?></div>
+                <div style="padding: 20px">**** <?= $stripePaymentMethod->card->last4; ?></div>
+                <div style="padding: 20px">Exp Date - <?= $stripePaymentMethod->card->exp_month . '/' . $stripePaymentMethod->card->exp_year; ?></div>
+                <div style="padding: 20px" >
                     <?php
                     // If it is not default allow deletion
                     if ($paymentMethod->default === \common\models\PaymentMethod::PRIMARY_PAYMENT_METHOD_NO) {
-                       echo Html::a('Delete', ['delete', 'id' => $paymentMethod->id], [
+                        echo Html::a('Delete', ['delete', 'id' => $paymentMethod->id], [
                             'class' => 'btn btn-danger',
                             'data' => [
                                 'confirm' => 'Are you sure you want to delete this payment method?',
@@ -49,24 +43,30 @@ $this->params['breadcrumbs'][] = $this->title;
                     }
                     ?>
 
-                <div>
-                    <?php
-                    //Mark the card as default for payment
-                    echo Html::a('Make Default',['select', 'id' => $paymentMethod->id]);
-                    ?>
+                    <div>
+                        <?php
+                        //Mark the card as default for payment
+                       //echo Html::a('Make Default', ['select', 'id' => $paymentMethod->id]);
+                        if ($paymentMethod->default === \common\models\PaymentMethod::PRIMARY_PAYMENT_METHOD_YES) { ?>
+                            <span class="btn btn-md btn-primary" disabled="disabled">Default</span>
+                            <?php
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
-        <?php
+            <?php
         }
-    ?>
-        <p style="text-align: right">
+        ?>
+        <p  style= "text-align: center">
             <?= Html::a('Add a Credit Card ', ['create'], ['class' => 'btn btn-success']) ?>
         </p>
     </div>
-
-
-
-    <?php
+    <div style="padding: 40px">
+    <div>
+        <h2 style="text-align:center">Invoices</h2>
+    </div>
+        <?php
 
     // Invoices
     echo GridView::widget([
@@ -82,4 +82,5 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+    </div>
 </div>
