65ace616f8afaf5bdf9ff8b0f9aa753c8f969950
Chris Smith <chris@cgsmith.net>
2020-07-06 19:25:27 -0500

    moving pagination to a base controller

---
+++ api/modules/v1/components/PaginatedControllerEx.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace api\modules\v1\components;
+
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class PaginatedControllerEx
+ *
+ * @package api\modules\v1\components
+ */
+class PaginatedControllerEx extends ControllerEx
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'Pagination' => [
+                'class' => 'api\modules\v1\components\parameters\Pagination',
+            ],
+            'Search'     => 'api\modules\v1\components\parameters\Search',
+            'Limit'      => 'api\modules\v1\components\parameters\Limit',
+        ]);
+    }
+}
