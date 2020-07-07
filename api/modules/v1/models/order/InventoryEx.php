69d84cb767f0af09bcebbdf75909b436ef25cd80
Chris Smith <chris@cgsmith.net>
2020-07-06 19:24:35 -0500

    renaming inventoryex to match others. rearranged fields for API to return them in a readable manner

--- api/modules/v1/models/order/InventoryEx.php
+++ api/modules/v1/models/order/InventoryEx.php
@@ -2,21 +2,19 @@
 
 namespace api\modules\v1\models\order;
 
-use common\models\base\BaseInventory;
+use common\models\Inventory;
 
 /**
  * Class ItemEx
  *
  * @package api\modules\v1\models\order
  */
-class InventoryEX extends BaseInventory
+class InventoryEx extends Inventory
 {
 
     /**
      * @SWG\Definition(
      *     definition = "Inventory",
-     *
-     *     @SWG\Property( property = "id", type = "integer", description = "ID" ),
      *
      *     @SWG\Property( property = "available_quantity", type = "number", description = "Available Quantity" ),
      *     @SWG\Property( property = "sku", type = "string", description = "SKU" ),
@@ -29,6 +27,6 @@ class InventoryEX extends BaseInventory
      */
     public function fields()
     {
-        return ['id', 'customer_id', 'available_quantity', 'sku', 'name'];
+        return ['id', 'sku', 'name', 'available_quantity'];
     }
 }
