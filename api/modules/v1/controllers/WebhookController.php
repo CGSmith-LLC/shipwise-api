7a3ffc2d1ae49e3e8b9aaf980511a99fe934d634
Chris Smith <chris@cgsmith.net>
2020-08-19 21:27:45 -0500

    remove unused code

--- api/modules/v1/controllers/WebhookController.php
+++ api/modules/v1/controllers/WebhookController.php
@@ -38,13 +38,6 @@ class WebhookController extends ControllerEx
      */
     public function actionIndex()
     {
-        //$verified = $this->verifyWebhook();
-        //$verified = $this->verifyWebhook();
-        //  var_export($verified, true);
-        //  die;
-        //throw new NotFoundHttpException('Unsupported action request.')
-
-
         $headers = Yii::$app->request->headers;
 
         $domain = $headers->get('x-shopify-shop-domain');
@@ -85,35 +78,6 @@ class WebhookController extends ControllerEx
         }
     }
 
-
-    /**
-     * @throws NotFoundHttpException
-     */
-    public function actionCreate()
-    {
-        $verified = $this->verifyWebhook();
-        var_export($verified, true);
-        die;
-        //throw new NotFoundHttpException('Unsupported action request.');
-    }
-
-    protected function verifyWebhook()
-    {
-        $shopifySharedSecret = CustomerMeta::find()->where([
-            'customer_id' => \Yii::$app->user->identity->customer->id,
-            'key' => 'shopify_shared_secret',
-        ])->one();
-
-        return hash_equals(\Yii::$app->request->headers->get('HTTP_X_SHOPIFY_HMAC_SHA256'),
-            base64_encode(hash_hmac('sha256', \Yii::$app->getRequest()
-                ->getRawBody(), $shopifySharedSecret->value, true)));
-    }
-
-    public function actionShopifyverify()
-    {
-        $verified = $this->verifyWebhook();
-        error_log('Webhook verified: ' . var_export($verified, true)); //check error.log to see the result
-    }
 
 
     public function behaviors()
