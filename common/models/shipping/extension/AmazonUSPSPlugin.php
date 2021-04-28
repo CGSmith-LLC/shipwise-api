3a2295a4b8a647a68722a316e52d69afba9e8d9b
Chris Wieber <chris@errigal.media>
2021-04-28 12:22:02 -0500

    Add extesion for Amazon USPS

---
+++ common/models/shipping/extension/AmazonUSPSPlugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace common\models\shipping\extension;
+
+/**
+ * Class AmazonUPSPlugin
+ *
+ * @package common\models\shipping\extension
+ */
+class AmazonUSPSPlugin extends AmazonMWS
+{
+
+    /**
+     * Plugin Name
+     *
+     * @var string Constant
+     */
+    const PLUGIN_NAME = "AmazonUSPS";
+}
