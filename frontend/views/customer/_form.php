264337f3668aaa082ced9768db9cc3f5426854b7
Cole <cole.hansen.4856@gmail.com>
2021-06-10 11:00:02 -0500

    GII generated controller, crud, and views

---
+++ frontend/views/customer/_form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Customer */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customer-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address1')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address2')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'state_id')->textInput() ?>
+
+    <?= $form->field($model, 'zip')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'created_date')->textInput() ?>
+
+    <?= $form->field($model, 'stripe_customer_id')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
