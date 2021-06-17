c223ce611cc71653de118a855e25b28c6282df88
Chris Smith <chris@cgsmith.net>
2021-06-17 11:01:02 -0500

    documentation link

--- frontend/views/api-consumer/index.php
+++ frontend/views/api-consumer/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'API Keys';
+$this->title = 'API';
 $this->params['breadcrumbs'][] = $this->title;
 if ((!Yii::$app->user->identity->getIsAdmin())) {
     $customerDropdownList = Yii::$app->user->identity->getCustomerList();
@@ -21,6 +21,7 @@ if ((!Yii::$app->user->identity->getIsAdmin())) {
 
     <p>
         <?= Html::a('Create API Key', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('<i class="fa fa-book"></i> Documentation', \yii\helpers\Url::to('https://api.getshipwise.com'), ['target' => '_blank', 'class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
