774e133dc98173684d7d2bf0fbc67f69eab5577c
Cole Hansen <cole@cgsmith.net>
2021-07-30 10:09:17 -0500

    added excluded items to forms and views

--- frontend/views/sku/_form.php
+++ frontend/views/sku/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use frontend\assets\ToggleAsset;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -7,6 +8,9 @@ use yii\widgets\ActiveForm;
 /* @var $model common\models\Sku */
 /* @var $form yii\widgets\ActiveForm */
 /* @var $customers array of customer ids */
+
+ToggleAsset::register($this);
+
 ?>
 
 <div class="sku-form">
@@ -17,11 +21,19 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <label>Is This An Excluded Item?</label>
+    <?= $form->field($model, 'excluded')->checkbox([
+        'data-toggle' => 'toggle',
+        'data-on' => 'Yes',
+        'data-off' => 'No',
+        'label' => false,
+    ]); ?>
+
     <?= $form->field($model, 'substitute_1')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'substitute_2')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'substitute_3')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'customer_id' )->dropDownList($customers, ['prompt' => 'Please Select'])?>
+    <?= $form->field($model, 'customer_id')->dropDownList($customers, ['prompt' => 'Please Select']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
--- frontend/views/sku/view.php
+++ frontend/views/sku/view.php
@@ -31,6 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             'sku',
             'name',
+            'excluded:boolean',
             [
                 'attribute' => 'customer',
                 'value' => $model->customer->name,
