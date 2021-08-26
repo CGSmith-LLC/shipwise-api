843a2396c200ac3128d88e2587c6518df9ffd606
Chris Smith <chris@cgsmith.net>
2021-08-25 22:52:03 -0500

    got testing working again

---
+++ console/tests/unit/InvoiceControllerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace console\tests\unit;
+
+
+class InvoiceControllerTest extends \Codeception\Test\Unit
+{
+
+}
--- frontend/tests/acceptance/NotSignedInCest.php
+++ frontend/tests/acceptance/NotSignedInCest.php
@@ -1,5 +1,6 @@
 <?php
 
+use frontend\tests\AcceptanceTester;
 /**
  * Class NotSignedInCest
  */
