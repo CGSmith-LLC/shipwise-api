50e042bd5c4524c9c990030f39688dbf22ae9b35
Karim Kessentini <programmaholic@gmail.com>
2021-10-28 14:04:09 -0700

    exception handle for bad input of customer_reference

--- frontend/models/search/OrderSearch.php
+++ frontend/models/search/OrderSearch.php
@@ -174,6 +174,7 @@ class OrderSearch extends Order
             ->andFilterWhere(['like', 'origin', $this->origin])
             ->andFilterWhere(['like', Address::tableName() . '.name', $this->address]);
 
+        $this->customer_reference = preg_replace("/[^a-zA-Z0-9]/", "", $this->customer_reference);
         if (!empty($this->customer_reference)) {
             $query->andWhere("MATCH(customer_reference) AGAINST ('*$this->customer_reference*' IN BOOLEAN MODE)");
         }
