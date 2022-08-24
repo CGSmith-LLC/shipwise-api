22755ba171228085e564eb580c55d44f6b0bf001
Chris Smith <chris@cgsmith.net>
2022-08-24 10:59:13 -0500

    update env variables

--- environments/dev/frontend/config/params-local.php
+++ environments/dev/frontend/config/params-local.php
@@ -1,4 +1,6 @@
 <?php
 return [
     'adminEmail'      => ['chris@cgsmith.net'],
+    'google_client_id'      => '877749800967-f715l3mfg7ma3kqpni24oskbokfbddns.apps.googleusercontent.com',
+    'google_client_secret'  => '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',
 ];
