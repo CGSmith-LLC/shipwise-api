04efad2a6fc8cbb5b599cee2150fd90a8272bd7f
Chris Smith <chris@cgsmith.net>
2023-02-22 07:10:55 -0600

    updating form related to #151

--- frontend/views/api-consumer/_form.php
+++ frontend/views/api-consumer/_form.php
@@ -10,17 +10,24 @@ use yii\widgets\ActiveForm;
 
 <div class="api-consumer-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php
+    $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'label')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'user_id')
+        ->hiddenInput(['value' => Yii::$app->user->identity->getId()]) ?>
+
     <?= $form->field($model, 'customer_id')
-        ->dropdownList($customers, ['prompt' => ' Please select']) ?>
+        ->dropdownList($customers, ['prompt' => ' Please select'])
+        ->label(false);
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php
+    ActiveForm::end(); ?>
 
 </div>
