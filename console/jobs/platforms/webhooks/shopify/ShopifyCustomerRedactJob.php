492e73cdf378dc5c3b8aa03f033bbeb1cc7b7435
Bohdan <bohdan.vorona@gmail.com>
2023-03-13 17:59:31 +0200

    ShopifyCustomerRedactJob

--- console/jobs/platforms/webhooks/shopify/ShopifyCustomerRedactJob.php
+++ console/jobs/platforms/webhooks/shopify/ShopifyCustomerRedactJob.php
@@ -7,13 +7,16 @@ use console\jobs\platforms\webhooks\BaseWebhookProcessingJob;
 /**
  * Class ShopifyCustomerRedactJob
  * @package console\jobs\platforms\webhooks\shopify
+ * @see https://shopify.dev/docs/apps/webhooks/configuration/mandatory-webhooks#customers-redact
  */
 class ShopifyCustomerRedactJob extends BaseWebhookProcessingJob
 {
+    /**
+     * Since we don't save information (data) about Shopify customers, we skip this mandatory webhook
+     */
     public function execute($queue): void
     {
         parent::execute($queue);
-        echo " customer redact ";
         $this->ecommerceWebhook->setSuccess();
     }
 }
