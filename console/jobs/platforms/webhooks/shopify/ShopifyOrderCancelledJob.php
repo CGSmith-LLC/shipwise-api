370bb611ef6fe19c11211068df33bb128a01866f
Bohdan <bohdan.vorona@gmail.com>
2023-03-13 19:02:28 +0200

    ShopifyOrderCancelledJob

--- console/jobs/platforms/webhooks/shopify/ShopifyOrderCancelledJob.php
+++ console/jobs/platforms/webhooks/shopify/ShopifyOrderCancelledJob.php
@@ -2,18 +2,43 @@
 
 namespace console\jobs\platforms\webhooks\shopify;
 
+use common\models\Status;
 use console\jobs\platforms\webhooks\BaseWebhookProcessingJob;
+use yii\helpers\Json;
 
 /**
  * Class ShopifyOrderCancelledJob
  * @package console\jobs\platforms\webhooks\shopify
+ * @see https://shopify.dev/docs/api/admin-rest/2023-01/resources/webhook#event-topics-orders-cancelled
  */
 class ShopifyOrderCancelledJob extends BaseWebhookProcessingJob
 {
     public function execute($queue): void
     {
         parent::execute($queue);
-        echo " cancelled ";
-        $this->ecommerceWebhook->setSuccess();
+
+        if ($this->cancel()) {
+            $this->ecommerceWebhook->setSuccess();
+        } else {
+            $this->ecommerceWebhook->setFailed();
+        }
+    }
+
+    protected function cancel(): bool
+    {
+        $payload = Json::decode($this->ecommerceWebhook->payload);
+
+        if (isset($payload['id'])) {
+            $externalOrderId = (int)$payload['id'];
+            $internalOrder = $this->getOrderByExternalId($externalOrderId);
+
+            if ($internalOrder) {
+                $internalOrder->status_id = Status::CANCELLED;
+                $internalOrder->save();
+                return true;
+            }
+        }
+
+        return false;
     }
 }
