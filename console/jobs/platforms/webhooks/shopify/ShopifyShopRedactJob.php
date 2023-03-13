0f7a25abc347b3307930fd783a56e243b895a0d8
Bohdan <bohdan.vorona@gmail.com>
2023-03-13 17:54:25 +0200

    ShopifyShopRedactJob

--- console/jobs/platforms/webhooks/shopify/ShopifyShopRedactJob.php
+++ console/jobs/platforms/webhooks/shopify/ShopifyShopRedactJob.php
@@ -2,18 +2,47 @@
 
 namespace console\jobs\platforms\webhooks\shopify;
 
+use common\models\EcommerceIntegration;
 use console\jobs\platforms\webhooks\BaseWebhookProcessingJob;
+use yii\helpers\Json;
 
 /**
  * Class ShopifyShopRedactJob
  * @package console\jobs\platforms\webhooks\shopify
+ * @see https://shopify.dev/docs/apps/webhooks/configuration/mandatory-webhooks#shop-redact
  */
 class ShopifyShopRedactJob extends BaseWebhookProcessingJob
 {
     public function execute($queue): void
     {
         parent::execute($queue);
-        echo " shop redact ";
-        $this->ecommerceWebhook->setSuccess();
+
+        if ($this->deleteEcommerceIntegration()) {
+            $this->ecommerceWebhook->setSuccess();
+        } else {
+            $this->ecommerceWebhook->setFailed();
+        }
+    }
+
+    protected function deleteEcommerceIntegration(): bool
+    {
+        $payload = Json::decode($this->ecommerceWebhook->payload);
+
+        if (isset($payload['shop_domain'])) {
+            /**
+             * @var $ecommerceIntegration EcommerceIntegration
+             */
+            $ecommerceIntegration = EcommerceIntegration::find()
+                ->byMetaKey('shop_url', trim($payload['shop_domain']))
+                ->one();
+
+            if ($ecommerceIntegration) {
+                $ecommerceIntegration->disconnect();
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
