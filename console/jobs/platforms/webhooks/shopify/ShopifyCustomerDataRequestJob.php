682e02a347f1ade7fbf2e2e9962e5774ec5987a6
Bohdan <bohdan.vorona@gmail.com>
2023-03-13 18:03:25 +0200

    ShopifyCustomerDataRequestJob

--- console/jobs/platforms/webhooks/shopify/ShopifyCustomerDataRequestJob.php
+++ console/jobs/platforms/webhooks/shopify/ShopifyCustomerDataRequestJob.php
@@ -7,13 +7,22 @@ use console\jobs\platforms\webhooks\BaseWebhookProcessingJob;
 /**
  * Class ShopifyCustomerDataRequestJob
  * @package console\jobs\platforms\webhooks\shopify
+ * @see https://shopify.dev/docs/apps/webhooks/configuration/mandatory-webhooks#customers-data_request
  */
 class ShopifyCustomerDataRequestJob extends BaseWebhookProcessingJob
 {
+    /**
+     * Quote:
+     *
+     * The webhook contains the resource IDs of the customer data that you need to provide to the store owner.
+     * It's your responsibility to provide this data to the store owner directly.
+     * In some cases, a customer record contains only the customer's email address.
+     *
+     * It means that it's enough just to mark the webhook as `success` since the `payload` field consists of the needed resource IDs.
+     */
     public function execute($queue): void
     {
         parent::execute($queue);
-        echo " customer data request ";
         $this->ecommerceWebhook->setSuccess();
     }
 }
