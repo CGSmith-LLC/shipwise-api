6daa1ba1b1ed369a804607bf221deb3d3f903f71
Carsten Brandt <mail@cebe.cc>
2023-03-16 09:21:26 +0100

    add dummy tests for codeception to not fail

---
+++ tests/functional/DummyCest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class DummyCest
+{
+
+    public function _before(FunctionalTester $i)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $i)
+    {
+
+    }
+
+}
